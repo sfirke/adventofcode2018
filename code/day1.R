bf27aad361ee84f30b5b66e1be0ee59e63c27129
Sam Firke <samuel.firke@gmail.com>
2018-12-01 14:18:56 -0500

    add puzzle info

--- code/day1.R
+++ code/day1.R
@@ -1,14 +1,22 @@
 # Day 1
 
+# Setup
 library(pacman)
 p_load(readr, dplyr, janitor)
 
+
+# Part 1 ----------------
+
+# Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
+  
 x <- read_csv("data/day1_1.txt", col_names = FALSE) %>%
   pull(X1)
 
 sum(x)
 
-# Part 2
+# Part 2 ---------------------------------
+
+# What is the first frequency your device reaches twice, continuing in a loop until a cumsum repeats?
 
 x_orig <- x
 done <- FALSE
