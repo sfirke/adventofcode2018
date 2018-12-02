be971b14465d37b3e3bf2049179a1d971d479204
Sam Firke <samuel.firke@gmail.com>
2018-12-02 14:50:57 -0500

    day 2 part 2
    learned the purrr equivalent of a nested for loop

--- code/day2.R
+++ code/day2.R
@@ -25,6 +25,26 @@ repeats <- x %>%
   map_df(tabyl, n) %>% # count the frequencies
   tabyl(n) # count how often "2" or "3" appeared (ignore how many times it did)
 
-
-
 repeats$n_n[2] * repeats$n_n[3]
+
+# Part 2
+
+# function takes two vectors, compares to see if they meet answer condition
+# if yes, prints answer
+reveal_answer <- function(x, y){
+  if(
+    sum(x == y) == (length(x) - 1)
+  ) {
+    print(paste0(x[x == y], collapse = ""))
+  }
+}
+
+# test
+reveal_answer(c("a", "b", "c", "d", "e"),
+              c("a", "b", "c", "d", "f"))
+
+vecs <- x %>%
+  map(split_chars) %>%
+  map(as.vector)
+
+walk(vecs, function(x) map(vecs, function(y) reveal_answer(x, y)))
