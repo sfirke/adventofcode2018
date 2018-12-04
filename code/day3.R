329aa51a898f24cb831971604b1095efb73d7064
Sam Firke <samuel.firke@gmail.com>
2018-12-03 20:50:59 -0500

    day 3 part 2 - a snap!

--- code/day3.R
+++ code/day3.R
@@ -8,7 +8,7 @@ p_load(readr, dplyr, janitor, tidyr, stringr, purrr)
 # Part 1 ----------------
 
 # How many square inches are claimed 2+ times?
-# Layer them all as matrices!
+# Layer them all as 0/1 matrices
 
 d <- read_delim("data/day3_1.txt", delim = "@", col_names = FALSE) %>%
   rename(id = X1) %>%
@@ -19,7 +19,8 @@ create_matrix <- function(x, y, w, h){
   m <- matrix(rep(0, 1000000), 1000, 1000)
   right <- x + w
   bottom <- y + h
-  m[(y + 1):(bottom), ((x + 1):(right))] <- 1
+  m[(y + 1):(bottom), ((x + 1):(right))] <- 1 # confusing that y is specified first
+                                              # also that the +1 is needed
   m
 }
 
@@ -38,3 +39,16 @@ for(i in seq(nrow(d))){
 }
 
 sum(ans > 1)
+
+# Part 2
+# What claim doesn't overlap?
+
+check_claim <- function(x, y, h, w){
+  right <- x + w
+  bottom <- y + h
+  claim_zone <- ans[(y + 1):(bottom), ((x + 1):(right))]
+  all(claim_zone == 1)
+}
+
+# wrote this line all at once and it ran!
+which(pmap_lgl(list(d$x, d$y, d$height, d$width), check_claim))
