11706790c4d9dd4d313040f1d3f1a342c4fd9744
Sam Firke <samuel.firke@gmail.com>
2018-12-14 17:13:19 -0500

    ugh this Part 2 code is slow, maybe I *will* refactor
    broke the loop after several hours, 2.2 MM combos and it wasn't there yet...

--- code/day14.R
+++ code/day14.R
@@ -29,5 +29,43 @@ for(i in 1:077201 + 10){
   e2_place <- get_new_elf_place(e2_place, board[e2_place])
 }
 
-target <- 9
+target <- 077201
 paste0(board[(target + 1):(target + 10)], collapse = "") # 92111343151 was "wrong too high" - whoops one digit too many
+
+
+# Part 2
+
+target <- as.character(target)
+# find position
+# test:
+target <- 59414
+boundary <- numeric(0)
+for(j in 1:length(board)){
+  if(paste0(board[j:(j+nchar(target)-1)], collapse = "") == target){
+    boundary <- j - 1
+    break
+  }
+}
+boundary # yep 2018
+
+# Actual. Going to need a bigger run of numbers, hope I can brute force this
+# Let's set it to 10MM and I'll go do work for a while and come back to it.
+# I could rewrite the code to study the loop as it runs, but this is faster in terms of people time.
+# I guess growing the board vector with c() ... will be quite inefficient but again, don't want to refactor to
+  # preallocate "board" with NA and keep an index of where the technical end of the vector is at any moment 
+
+for(i in 1:10000000){
+  update_board(board[e1_place], board[e2_place])
+  e1_place <- get_new_elf_place(e1_place, board[e1_place])
+  e2_place <- get_new_elf_place(e2_place, board[e2_place])
+}
+
+target <- 077201
+boundary <- numeric(0)
+for(j in 1:length(board)){
+  if(paste0(board[j:(j+nchar(target)-1)], collapse = "") == target){
+    boundary <- j - 1
+    break
+  }
+}
+boundary
